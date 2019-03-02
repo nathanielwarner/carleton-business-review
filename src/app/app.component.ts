60cafe7caacfc66a5b08f2d040fc9800dc257ef4
Nathaniel Warner <nathanielwarner77@gmail.com>
2019-03-01 22:44:57 -0600

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'carleton-biz';
+  title = 'Carleton Investment Group';
 }
